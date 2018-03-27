cd5c85240aed45fc03a72c0bc22ea6c70c48b7cf
Murat Celep <mcelep@mcelep-OSX.local>
2018-03-27 18:39:46 +0300

    prepare prod deployments

--- prod/provision.sh
+++ prod/provision.sh
@@ -7,9 +7,15 @@ oc project $CICD_PROD
 oc create -f cm_mongodb.yml && oc new-app -f ss_mongodb.yml
 
 #NationalParks
+oc new-app -f ../dev/template_nationalparks.yml
+oc patch dc/nationalparks --type=json \
+ -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/5", "value":{"name": "DB_REPLICASET","valueFrom": {"configMapKeyRef": {"key": "database-rs","name": "mongodb-configmap"}}} }]'
 
 #MLBParks
+oc new-app -f ../dev/template_mlbparks.yml
+oc patch dc/mlbparks --type=json \
+ -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/5", "value":{"name": "DB_REPLICASET","valueFrom": {"configMapKeyRef": {"key": "database-rs","name": "mongodb-configmap"}}} }]'
 
 
 #Parks
-#oc new-app -f  ../dev/template_parks.yml
+oc new-app -f ../dev/template_parks.yml
