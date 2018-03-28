7ac99c77c88fed1b8cbfe9308e644c5895324e81
Cloud User <ec2-user@ip-10-0-0-107.ec2.internal>
2018-03-28 11:06:27 +0000

    use skopeo instead of docker push

--- jenkins-slave-appdev/build.sh
+++ jenkins-slave-appdev/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -x -e
 source ../env.sh
-docker build . -t docker-registry-default.apps.na37.openshift.opentlc.com/$CICD_PROJECT/jenkins-slave-maven-appdev:v3.7
-docker login -u someUser -p $(oc whoami -t) docker-registry-default.apps.na37.openshift.opentlc.com
-docker push --disable-content-trust docker-registry-default.apps.na37.openshift.opentlc.com/$CICD_PROJECT/jenkins-slave-maven-appdev:v3.7
-oc label is/jenkins-slave-maven-appdev role jenkins-slave
+docker build . -t jenkins-slave-maven-appdev:v3.7
+skopeo copy --dest-tls-verify=false --dest-creds="$(oc whoami):$(oc whoami -t)" docker-daemon:jenkins-slave-maven-appdev:v3.7 docker://docker-registry-default.apps.na37.openshift.opentlc.com/$CICD_PROJECT/jenkins-slave-maven-appdev:v3.7
