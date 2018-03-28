b2c7f6900302ca5e6293ab253534c63d6fd6040a
Murat Celep <mcelep@mcelep-OSX.local>
2018-03-28 12:02:13 +0300

    ignore certificate for docker push

--- jenkins-slave-appdev/build.sh
+++ jenkins-slave-appdev/build.sh
@@ -3,5 +3,5 @@ set -x -e
 source ../env.sh
 docker build . -t docker-registry-default.apps.na37.openshift.opentlc.com/$CICD_PROJECT/jenkins-slave-maven-appdev:v3.7
 docker login -u someUser -p $(oc whoami -t) docker-registry-default.apps.na37.openshift.opentlc.com
-docker push docker-registry-default.apps.na37.openshift.opentlc.com/$CICD_PROJECT/jenkins-slave-maven-appdev:v3.7
+docker push --disable-content-trust docker-registry-default.apps.na37.openshift.opentlc.com/$CICD_PROJECT/jenkins-slave-maven-appdev:v3.7
 oc label is/jenkins-slave-maven-appdev role jenkins-slave
