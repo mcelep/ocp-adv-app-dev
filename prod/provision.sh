0fb81ad11881e01c67dcb9e17ed3098032f6a733
Murat Celep <mcelep@mcelep-OSX.fritz.box>
2018-04-02 17:06:45 +0200

    bugfix

--- prod/provision.sh
+++ prod/provision.sh
@@ -7,16 +7,27 @@ oc project $CICD_PROD
 oc create -f cm_mongodb.yml && oc new-app -f ss_mongodb.yml
 
 #NationalParks
-oc new-app -f ../dev/template_nationalparks.yml
-oc patch dc/nationalparks --type=json \
+oc new-app -f ../dev/template_nationalparks.yml -p APP_NAME=nationalparks-blue
+oc patch dc/nationalparks-blue --type=json \
  -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/5", "value":{"name": "DB_REPLICASET","valueFrom": {"configMapKeyRef": {"key": "database-rs","name": "mongodb-configmap"}}} }]'
 
-#MLBParks
-oc new-app -f ../dev/template_mlbparks.yml
-oc patch dc/mlbparks --type=json \
+oc new-app -f ../dev/template_nationalparks.yml -p APP_NAME=nationalparks-green
+oc patch dc/nationalparks-green --type=json \
  -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/5", "value":{"name": "DB_REPLICASET","valueFrom": {"configMapKeyRef": {"key": "database-rs","name": "mongodb-configmap"}}} }]'
+
+
+#MLBParks
+oc new-app -f ../dev/template_mlbparks.yml -p APP_NAME=mlbparks-blue
+oc patch dc/mlbparks-blue --type=json \
+ -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/5", "value":{"name": "DB_REPLICASET","valueFrom": {"configMapKeyRef": {"key": "database-rs","name": "mongodb-configmap"}}} }]'
+
+oc new-app -f ../dev/template_mlbparks.yml -p APP_NAME=mlbparks-green
+oc patch dc/mlbparks-green --type=json \
+ -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/5", "value":{"name": "DB_REPLICASET","valueFrom": {"configMapKeyRef": {"key": "database-rs","name": "mongodb-configmap"}}} }]'
+
 
 
 #Parksmap
-oc new-app -f ../dev/template_parksmap.yml  -p APP_NAME=parksmap-green
 oc new-app -f ../dev/template_parksmap.yml  -p APP_NAME=parksmap-blue
+oc new-app -f ../dev/template_parksmap.yml  -p APP_NAME=parksmap-green
+
